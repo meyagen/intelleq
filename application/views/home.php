846cf0490e4a4a3044b0ec66f90ed23b216a11ae
Kristoffer Mendoza <kristoffer_mlmendoza@yahoo.com>
2013-09-28 18:53:08 +0800

    Logo for navbar upper left fixed

--- application/views/home.php
+++ application/views/home.php
@@ -4,9 +4,15 @@
 <header>
 <div class="fixed">
 <nav class="top-bar fixed">
-  <ul class="title-area">
-    <li class="name"><h1><a href="">intelleq</a></h1></li>
-    <!--li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li-->
+  <section class="top-bar-section">
+    <ul class="left">
+
+    </ul>
+  </section>
+  <ul class="title-area" style="display:inline">
+    <!--li class="toggle-topbar menu-icon right" style="padding-top:22px"><a href="#"><span></span></a></li-->
+    <li class="name"><a href="#" class="icon"> </a><h1><a href="#" style="display:inline">intelleq</a></h1></li>
+    
   </ul>
   <section class="top-bar-section">
     <ul class="right"style="background:rgba(105, 34, 34, 0)">
